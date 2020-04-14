d0391242bae8b465fc304abf6af60247a291c0f2
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2020-04-14 16:21:10 +0200

    fix(mahlzeit)

--- default.nix
+++ default.nix
@@ -8,7 +8,7 @@
     rev = "2a5129bbb6a4f24e02a9bc0c73cbd8b5e454c9be";
     sha256 = "1pydx4a00hssbgcyyasxrihhr344hcrs95xwlz0gj14w50d9wabm";
   }) {};
-  mahlzeit = pkgs.haskellPackages.callPackage (pkgs.fetchFromGitHub {
+  mahlzeit = pkgs.haskellPackages.callCabal2nix "mahlzeit" (pkgs.fetchFromGitHub {
     owner = "kmein";
     repo = "mahlzeit";
     rev = "954c0fb3f45815999bc65d003794af6a850b069c";
