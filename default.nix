7c6b9423aae8cd00ce57005e1913b3bd644bff96
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2020-05-12 22:22:05 +0200

    fix: add as submodules

--- default.nix
+++ default.nix
@@ -10,12 +10,7 @@
     sha256 = "1pydx4a00hssbgcyyasxrihhr344hcrs95xwlz0gj14w50d9wabm";
   }) {};
   */
-  mahlzeit = pkgs.haskellPackages.callCabal2nix "mahlzeit" (pkgs.fetchFromGitHub {
-    owner = "kmein";
-    repo = "mahlzeit";
-    rev = "954c0fb3f45815999bc65d003794af6a850b069c";
-    sha256 = "046yrr40hjmxkjmwzcvmwb39fxx2v2i6hgdxrjfiwilzvhikarrg";
-  }) {};
+  mahlzeit = pkgs.haskellPackages.callCabal2nix "mahlzeit" ./mahlzeit {};
   python3Packages = {
     sncli = pkgs.python3Packages.callPackage ./sncli {};
     spotify-cli-linux = pkgs.pytohn3Packages.callPackage ./spotify-cli-linux {};
